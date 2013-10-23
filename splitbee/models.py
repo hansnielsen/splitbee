fdabff1a68ff61826b4e5590dc9f25ef574eb3a0
Andrew Dunham <andrew@du.nham.ca>
2013-10-22 17:44:57 -0700

    Testing

--- splitbee/models.py
+++ splitbee/models.py
@@ -1,3 +1,5 @@
+import string
+import random
 from decimal import Decimal
 
 from sqlalchemy import (
@@ -10,15 +12,23 @@ from sqlalchemy import (
     Table,
 )
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import types
 
 from .config import DEBUG
 
 Base = declarative_base()
-engine = create_engine('sqlite:///test.db')
-create_session = sessionmaker(bind=engine)
+Session = sessionmaker()
+
+
+ID_TRANSLATION = ''.join([chr(x) for x in range(256)])
+DEFAULT_ALPHABET = (string.ascii_letters + string.digits).translate(
+    ID_TRANSLATION,
+    'oO0Il1'
+)
+def gen_random(length, alphabet=DEFAULT_ALPHABET):
+    return ''.join(random.choice(alphabet) for x in range(length))
 
 
 class StringCurrency(types.TypeDecorator):
@@ -29,32 +39,46 @@ class StringCurrency(types.TypeDecorator):
         return dialect.type_descriptor(types.VARCHAR(100))
 
     def process_bind_param(self, value, dialect):
+        if isinstance(value, float):
+            value = Decimal(value)
+
         return str(value.quantize(self.precision))
 
     def process_result_value(self, value, dialect):
         return Decimal(value)
 
 
-class BillPayer(Base):
-    __tablename__ = 'bill_payers'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    bill_id = Column(Integer, ForeignKey('bills.id'), primary_key=True)
-
-    amount_paid = 
-
-
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
     email = Column(String)
-    # TODO: persona auth
+    # auth
 
-    bills_paid = relationship('Bill', backref='payer')
-    bills_split = relationship('Bill',
-                               secondary=bill_user_assoc_table,
-                               backref='members')
+    # Optional name
+    name = Column(String, nullable=True)
+
+    def __init__(self, email, password, name=None):
+        self.email = email
+        #self.password = password
+        self.name = name
+
+    @property
+    def total_owing(self):
+        """
+        Total amount this user owes - i.e. the amount they would have paid
+        if all bills were split perfectly and they paid for their entire share.
+        """
+        # TODO: this should use sql, not sum here
+        return sum(x.amount_paid for x in self.paid_components)
+
+    @property
+    def total_paid(self):
+        """
+        Total amount this user has actually paid for.
+        """
+        # TODO: this should use sql, not sum here
+        return 0
 
     def __repr__(self):
         return "<User(%d, '%s')>" % (self.id, self.name)
@@ -67,15 +91,63 @@ class Bill(Base):
     """
     __tablename__ = 'bills'
 
-    id = Column(Integer, primary_key=True)
-    description = Column(String(140))
+    # NOTE: This ID can't be an integer, since it will be displayed to end-
+    # users and used to look up bills.  We make it a random string do
+    # dissuade brute-forcing.
+    id = Column(String(8), primary_key=True)
+    description = Column(String(140), nullable=False)
 
-    # Total amount of the bill
-    total_amount = Column(StringCurrency, nullable=False)
+    # ID of the payer.  NULL means "not yet paid"
+    payer_id = Column(Integer, ForeignKey('users.id'))
 
-    # ID of the payer
-    payer_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    def __init__(self, description=""):
+        self.id = gen_random(8)
+        self.description = description
+
+    @property
+    def is_paid(self):
+        return self.payer_id is not None
+
+    @property
+    def total_amount(self):
+        # TODO: this should use sql, not sum here
+        return sum(x.amount_paid for x in self.bill_components)
 
     def __repr__(self):
         return "<Bill(id=%d, total_amount='%s')>" % (self.id,
                                                      self.total_amount)
+
+
+class BillComponent(Base):
+    """
+    A component of a bill that is owned by a certain user.
+    """
+    __tablename__ = 'bill_components'
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    bill_id = Column(Integer, ForeignKey('bills.id'), primary_key=True)
+
+    amount_paid = Column(StringCurrency, nullable=False)
+
+    # Bidirectional relationship to User
+    payer = relationship(User,
+                backref=backref("paid_components",
+                                cascade="all, delete-orphan")
+                )
+
+    # Reference to bill
+    bill = relationship(Bill, backref=backref("bill_components",
+                                              cascade="all, delete-orphan"))
+
+    def __init__(self, bill, payer, amount):
+        self.bill = bill
+        self.payer = payer
+        self.amount_paid = amount
+
+
+def bind_db(conn_str, create=False):
+    engine = create_engine(conn_str)
+    Session.configure(bind=engine)
+    if create:
+        Base.metadata.create_all(engine)
+
+    return engine
